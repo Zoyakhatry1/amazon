c6564f2062b1da217e77b85823832ca99b9ddfbd
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2022-02-18 20:49:42 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home(props) {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      
+      <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        {/* <ProductFeed products={props.products}/> */}
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context){
+  const products = await fetch("https://fakestoreapi.com/products").then(
+   (res)=> res.json()
+  );    
+
+  return{
+    props:{
+      products,
+    }
+  }
+}
